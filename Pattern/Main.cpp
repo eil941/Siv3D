938b5c623002b85a3f717b44bf02fe8ccee34bc1
ei1941 <hiroy@DESKTOP-8TGG3F2>
2025-10-17 16:01:48 +0900

    模様について

--- Pattern/Main.cpp
+++ Pattern/Main.cpp
@@ -1,97 +1,177 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.16
 
-void Main()
-{
-	// 背景の色を設定する | Set the background color
-	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
-
-	// 画像ファイルからテクスチャを作成する | Create a texture from an image file
-	const Texture texture{ U"example/windmill.png" };
-
-	// 絵文字からテクスチャを作成する | Create a texture from an emoji
-	const Texture emoji{ U"🦖"_emoji };
-
-	// 太文字のフォントを作成する | Create a bold font with MSDF method
-	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
-
-	// テキストに含まれる絵文字のためのフォントを作成し、font に追加する | Create a font for emojis in text and add it to font as a fallback
-	const Font emojiFont{ 48, Typeface::ColorEmoji };
-	font.addFallback(emojiFont);
-
-	// ボタンを押した回数 | Number of button presses
-	int32 count = 0;
-
-	// チェックボックスの状態 | Checkbox state
-	bool checked = false;
-
-	// プレイヤーの移動スピード | Player's movement speed
-	double speed = 200.0;
-
-	// プレイヤーの X 座標 | Player's X position
-	double playerPosX = 400;
-
-	// プレイヤーが右を向いているか | Whether player is facing right
-	bool isPlayerFacingRight = true;
+//円を並べる
+void lineup() {
+	Scene::SetBackground(Palette::White);
 
 	while (System::Update())
 	{
-		// テクスチャを描く | Draw the texture
-		texture.draw(20, 20);
-
-		// テキストを描く | Draw text
-		font(U"Hello, Siv3D!🎮").draw(64, Vec2{ 20, 340 }, ColorF{ 0.2, 0.4, 0.8 });
-
-		// 指定した範囲内にテキストを描く | Draw text within a specified area
-		font(U"Siv3D (シブスリーディー) は、ゲームやアプリを楽しく簡単な C++ コードで開発できるフレームワークです。")
-			.draw(18, Rect{ 20, 430, 480, 200 }, Palette::Black);
-
-		// 長方形を描く | Draw a rectangle
-		Rect{ 540, 20, 80, 80 }.draw();
-
-		// 角丸長方形を描く | Draw a rounded rectangle
-		RoundRect{ 680, 20, 80, 200, 20 }.draw(ColorF{ 0.0, 0.4, 0.6 });
-
-		// 円を描く | Draw a circle
-		Circle{ 580, 180, 40 }.draw(Palette::Seagreen);
-
-		// 矢印を描く | Draw an arrow
-		Line{ 540, 330, 760, 260 }.drawArrow(8, SizeF{ 20, 20 }, ColorF{ 0.4 });
-
-		// 半透明の円を描く | Draw a semi-transparent circle
-		Circle{ Cursor::Pos(), 40 }.draw(ColorF{ 1.0, 0.0, 0.0, 0.5 });
-
-		// ボタン | Button
-		if (SimpleGUI::Button(U"count: {}"_fmt(count), Vec2{ 520, 370 }, 120, (checked == false)))
+		for (int32 i = 0; i < 6; ++i)
 		{
-			// カウントを増やす | Increase the count
-			++count;
+			Circle{ (i * 100), 100, 30 }.draw(Palette::Skyblue);
 		}
 
-		// チェックボックス | Checkbox
-		SimpleGUI::CheckBox(checked, U"Lock \U000F033E", Vec2{ 660, 370 }, 120);
-
-		// スライダー | Slider
-		SimpleGUI::Slider(U"speed: {:.1f}"_fmt(speed), speed, 100, 400, Vec2{ 520, 420 }, 140, 120);
-
-		// 左キーが押されていたら | If left key is pressed
-		if (KeyLeft.pressed())
+		for (int32 i = 0; i < 6; ++i)
 		{
-			// プレイヤーが左に移動する | Player moves left
-			playerPosX = Max((playerPosX - speed * Scene::DeltaTime()), 60.0);
-			isPlayerFacingRight = false;
+			Circle{ (50 + i * 100), 200, 30 }.draw(Palette::Seagreen);
 		}
-
-		// 右キーが押されていたら | If right key is pressed
-		if (KeyRight.pressed())
-		{
-			// プレイヤーが右に移動する | Player moves right
-			playerPosX = Min((playerPosX + speed * Scene::DeltaTime()), 740.0);
-			isPlayerFacingRight = true;
-		}
-
-		// プレイヤーを描く | Draw the player
-		emoji.scaled(0.75).mirrored(isPlayerFacingRight).drawAt(playerPosX, 540);
 	}
+}
+//二重ループで円を並べる
+void lineup2() {
+	Scene::SetBackground(Palette::White);
+
+	while (System::Update())
+	{
+		for (int32 y = 0; y < 40; ++y) // 縦方向
+		{
+			for (int32 x = 0; x < 60; ++x) // 横方向
+			{
+				Circle{ (x * 100), (y * 100), 30 }.draw(Palette::Skyblue);
+			}
+		}
+	}
+}
+//少し複雑に
+void lineup3() {
+	Scene::SetBackground(Palette::White);
+
+	while (System::Update())
+	{
+		for (int32 y = 0; y < 40; ++y)
+		{
+			for (int32 x = 0; x < 60; ++x)
+			{
+				if (IsEven(x + y)) // x + y が偶数なら
+				{
+					// 大きい円を描く
+					Circle{ (x * 100), (y * 100), 30 }.draw(Palette::Skyblue);
+				}
+				else // 奇数なら
+				{
+					// 小さい円を描く
+					Circle{ (x * 100), (y * 100), 10 }.draw(Palette::Skyblue);
+				}
+			}
+		}
+	}
+}
+
+//長方形を並べる
+void lineupRect() {
+	Scene::SetBackground(Palette::White);
+
+	while (System::Update())
+	{
+		for (int32 x = 0; x < 60; ++x)
+		{
+			Rect{ (x * 100), 0, 80, 600 }.draw(Palette::Skyblue);
+		}
+	}
+}
+//サイズを徐々に変える
+void lineupReact2() {
+	Scene::SetBackground(Palette::White);
+
+	while (System::Update())
+	{
+		for (int32 x = 0; x < 60; ++x)
+		{
+			Rect{ (x * 100), 0, 80, ((x + 1) * 100) }.draw(Palette::Skyblue);
+		}
+	}
+}
+//色を徐々に変える
+void lineupRect3() {
+	Scene::SetBackground(Palette::White);
+
+	while (System::Update())
+	{
+		for (int32 x = 0; x < 60; ++x)
+		{
+			Rect{ (x * 100), 0, 80, 600 }.draw(ColorF{ 0.0, (x * 0.15), (x * 0.15) });
+		}
+	}
+}
+//色相を徐々に変える
+void lineupRect4() {
+	Scene::SetBackground(Palette::White);
+
+	while (System::Update())
+	{
+		for (int32 x = 0; x < 10; ++x)
+		{
+			Rect{ (x * 80), 0, 80, 600 }.draw(HSV{ (x * 40), 0.5, 1.0 });
+		}
+	}
+}
+
+//波紋
+void ripples() {
+	Scene::SetBackground(Palette::White);
+
+	while (System::Update())
+	{
+		for (int32 i = 0; i < 60; ++i)
+		{
+			Circle{  100, 300, (i * 10) }.drawFrame(4, Palette::Skyblue);
+		}
+	}
+}
+//市松模様
+void checkerboard() {
+	Scene::SetBackground(Palette::White);
+	while (System::Update())
+	{
+		for (int32 i = 0; i < 10; i++) {
+			for (int32 j = 0; j < 10; j++) {
+				Rect(i * 60, j * 60, 60,60).draw(((i + j)%2==0) ? Palette::Black : Palette::White);
+			}
+		}
+	}
+}
+//マス目
+void grid() {
+	Scene::SetBackground(Palette::White);
+	constexpr int32 cellSize = 40; // マスのサイズ
+	constexpr int32 cols = 60;    // 列数
+	constexpr int32 rows = 40;    // 行数
+	while (System::Update())
+	{
+		for (int32 y = 0; y < rows; ++y)
+		{
+			for (int32 x = 0; x < cols; ++x)
+			{
+				Rect{ x * cellSize, y * cellSize, cellSize, cellSize }.drawFrame(1, Palette::Black);
+			}
+		}
+	}
+}
+
+void Main()
+{
+	//円を並べる
+	//lineup();
+	//二重ループで円を並べる
+	//lineup2();
+	//少し複雑に
+	//lineup3();
+
+	//長方形を並べる
+	//lineupRect();
+	//サイズを徐々に変える
+	//lineupReact2();
+	//色を徐々に変える
+	//lineupRect3();
+	//色相を徐々に変える
+	//lineupRect4();
+
+	//波紋
+	//ripples();
+	//市松模様
+	//checkerboard();
+	//マス目
+	grid();
 }
 
 //
